f5cd3925b8d1f5e7e3e8a553a90f93f701dc6b78
Ryan Lui <tomaster.ryan@gmail.com>
2016-07-18 20:23:13 -0700

    update Lesson5 labs

--- Lesson05/age-check/js/ageCheck.js
+++ Lesson05/age-check/js/ageCheck.js
@@ -10,6 +10,28 @@ var p      = document.querySelector('.age-check p');
 button.addEventListener('click', checkAge);
 
 
+function getPrivilege(age) {
+	var msg=""
+    if (age < 16 && age >=0) {
+         msg="You cannot do much outside of going to school !";
+    } else if (age >=16 && age < 18) {
+         msg="You can drive !";
+    } else if (age >=18 && age < 21) {
+         msg="You can vote !";
+    } else if (age >=21 && age < 25) {
+         msg="You can drink alcohol !";
+    } else if (age >=25 && age < 35) {
+         msg="You can rent a car !";
+    } else if (age >=35 && age < 62) {
+         msg="You can run for president !";
+    } else if (age >=62 ) {
+         msg="You collect social security benefits !";
+    } else  
+         msg="Wrong input, please try again !!";
+   return msg;
+}
+
+
 // Event Listeners
 // ------------------------------------------
 function checkAge(e) {
