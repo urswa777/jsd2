bcdef491f7f6d196088dd10dd0916053aa4c4d76
Ryan Lui <tomaster.ryan@gmail.com>
2016-08-13 15:33:06 -0700

    refresh homework06

--- homework06/js/movieSearch__SearchList_done.js
+++ homework06/js/movieSearch__SearchList_done.js
@@ -1,15 +1,9 @@
 // Setup
 // ----------------------------------------------
 var g_searchMovieTitle = ""; 
-var url_search_api   ="http://www.omdbapi.com/?s=";
-var url_oneMovie_api ="http://www.omdbapi.com/?i=";
-var url_search   ="";
-var url_oneMovie ="";
+var url_search ="";
 var one_movie_is_chosen=0;
 var defaultMoviePoster_img="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTc-k0F0Og_2TFjqrEy38pgK1-0_qnv7EVLWLCTlA_MaA7-1xI3nw";
-var DetailMovieId="";
-var movieCounter=0;
-
 // Structure
 // ----------------------------------------------
 var _form    = document.querySelector(".search_form");
@@ -75,6 +69,12 @@ function goSearch(e){
     searchMovieList(g_searchMovieTitle);
 
 	//------------------------------------------------------------
+	console.log('Step2 - Reset Detail region and display the top one! ');	
+
+	//------------------------------------------------------------
+	console.log('Step3 - Reset List region! ');	
+
+	//------------------------------------------------------------
 	console.log('Step4 - Display List of movies! ');	
     _search.value="";
 }
@@ -84,16 +84,33 @@ function goSearch(e){
 // ----------------------------------------------
 
 function searchMovieList(search_str){
-	console.log("Trigger action : Search Movies with string '"+search_str+"'");	
-    url_search = url_search_api + search_str;
+	console.log("Trigger action : Search Movie with string '"+search_str+"'");	
+    url_search = "http://www.omdbapi.com/?s="+search_str;
 	console.log("URL >> "+ url_search);	
-	$.getJSON(url_search, showSearchResult);
+	$.getJSON(url_search, showMovieList);
+    debugger
 }
 
 
 
+//  http://www.omdbapi.com/?s=boy
+function clearMovieListDisplay(movie_id){
+	console.log('Trigger action  : Clear Movie List Display! ');	
+}
 
-function showSearchResult(list){
+function clearMovieDetailDisplay(movie_id){
+	console.log('Trigger action  : Clear Movie Detail Display! ');	
+}
+
+function search1MovieDetail(movie_id){
+	console.log('Trigger action  : Show one Movie detail! ');	
+}
+
+function showOneMovieDetail(oneMovie){
+	console.log('-- Trigger action  : Show one Movie detail! ');	
+}
+
+function showMovieList(list){
     var searchSummary_HTML="";
     var movieList = list.Search;
 	console.log('-- Trigger action  : Show search result (movie list) ');	
@@ -107,11 +124,8 @@ function showSearchResult(list){
     _searchSummary.innerHTML=searchSummary_HTML;
     clearDTLScreen();
     clearListScreen();
-    movieList.forEach(showOneMovieHeader); //--- show list of movies' header
-    movieList.forEach(show1stMovieDetail);
-    movieCounter=0;
+    movieList.forEach(showOneMovieHeader);
 }
-
 
 function showOneMovieHeader(OneMovie) {
 	var movieImgStr="";
@@ -147,90 +161,6 @@ function showOneMovieHeader(OneMovie) {
     _results.appendChild(li);
 }
 
-
-function show1stMovieDetail(OneMovie) {
-	var buf=0;
-	if (movieCounter>0) {
-	   console.log('Not First movie, ignore!')
-	   return;
-	}
-	console.log('Show First movie with ID' + OneMovie.imdbID)
-
-    DetailMovieId = OneMovie.imdbID;
-    buf=DetailMovieId.trim();
-    if (buf==0) {
-    	return;
-    }
-    searchMovieDetail(DetailMovieId);
-    
- 
-    /*
-	var movieImgStr="";
-	var movieTitle="";
-	var movieID="";
-    var li=document.createElement("li");
-    var img=document.createElement("img");
-    var p=document.createElement("p");
-    var buf="";
-    movieTitle= OneMovie.Title ;
-    movieImgStr = OneMovie.Poster;
-    movieID = OneMovie.imdbID;
-
-    buf=movieTitle.trim();
-    if (buf.length==0) {
-      return;
-    }
-    buf=movieID.trim();
-    if (buf.length==0) {
-      return;
-    }
-    buf=movieImgStr.trim();
-    if (buf.length==0) {
-      movieImgStr = defaultMoviePoster_img;
-    }
-    //---------------------------------------------------
-    li.id=movieID;
-    p.innerHTML= OneMovie.Title ;
-    img.src=movieImgStr;
-
-    li.appendChild(img);
-    li.appendChild(p);
-    _results.appendChild(li);
-
-    movieCounter+=1;
-    */
-    movieCounter+=1;
-}
-
-function searchMovieDetail(movie_id){
-	console.log("Trigger action : Search one Movie with imdb ID '"+movie_id+"'");	
-    url_oneMovie = url_oneMovie_api + movie_id;
-	console.log("URL >> "+ url_oneMovie);	
-	$.getJSON(url_oneMovie, showOneMovieDetail);
-}
-
-function showOneMovieDetail(OneMovieJson) {
-    console.log('showOneMovieDetail()');
-    console.log(OneMovieJson);
-	var moviePoster=OneMovieJson.Poster;
-	var movieTitle=OneMovieJson.Title;
-	var moviePlot=OneMovieJson.Plot;
-	var movieURL="http://www.imdb.com/title/"+OneMovieJson.imdbID;
-	var movieURL_desc="View on IMDB";
-    var buf="";
-
-    buf=moviePoster.trim();
-    if (buf.length==0) {
-      moviePoster = defaultMoviePoster_img;
-    }
-   
-    _poster.src=moviePoster;
-    _title.textContent= movieTitle ;
-    _plot.textContent= moviePlot ;
-    _url.textContent= movieURL_desc;
-    _url.href= movieURL ;
-    
-}
 
 // Update page
 // ----------------------------------------------
